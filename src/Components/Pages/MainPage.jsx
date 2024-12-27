c4188435a67812929ca81ca7eaea739a66f2eb9b
CemCanplt <cem--canpolat@hotmail.com>
2024-12-27 21:31:36 +0300

    Update MainPage.jsx

--- src/Components/Pages/MainPage.jsx
+++ src/Components/Pages/MainPage.jsx
@@ -5,7 +5,13 @@ function MainPage() {
     <div className="main-page">
       <header>
         <h1>Teknolojik Yemekler</h1>
+        <p>
+          KOD ACIKTIRIR
+          <br />
+          PİZZA, DOYURUR
+        </p>
       </header>
+      <button className="aciktim">ACIKTIM</button>
     </div>
   );
 }
