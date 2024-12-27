0a0a777e2f78b4943f0cfe88cb3d184abd27b470
CemCanplt <cem--canpolat@hotmail.com>
2024-12-27 20:38:03 +0300

    Font Ekledim

--- src/Components/Pages/MainPage.jsx
+++ src/Components/Pages/MainPage.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 function MainPage() {
     return (
         <div className="main-page">
+            <header><h1>Teknolojik Yemekler</h1></header>
         </div>
     )
 }
