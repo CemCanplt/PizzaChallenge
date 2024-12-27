70c26bd976c495deb650c2538eb2b4c9240421e1
CemCanplt <cem--canpolat@hotmail.com>
2024-12-27 23:39:54 +0300

    İlk sayfa devam

--- src/Components/Pages/MainPage.jsx
+++ src/Components/Pages/MainPage.jsx
@@ -3,17 +3,24 @@ import React from "react";
 function MainPage() {
   return (
     <div className="main-page">
-      <p>
-        KOD ACIKTIRIR
-        <br />
-        PİZZA, DOYURUR
-      </p>
-      <button
-        className="aciktim"
-        onClick={null /* sonra eklenecek handleClick */}
-      >
-        ACIKTIM
-      </button>
+      <div className="text-pic-gap">
+        <p>
+          KOD ACIKTIRIR
+          <br />
+          PİZZA, DOYURUR
+        </p>
+        <button
+          className="aciktim"
+          onClick={null /* sonra eklenecek handleClick */}
+        >
+          ACIKTIM
+        </button>
+      </div>
+      <img
+        src="../../images/pngegg3.png"
+        alt="Büyük Pizza"
+        className="big-pizza"
+      />
     </div>
   );
 }
