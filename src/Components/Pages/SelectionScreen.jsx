d5e00c363f83dd8a695f15dfc07a7a42dd195491
CemCanplt <cem--canpolat@hotmail.com>
2024-12-29 17:54:03 +0300

    Sipariş devam

--- src/Components/Pages/SelectionScreen.jsx
+++ src/Components/Pages/SelectionScreen.jsx
@@ -1,5 +1,27 @@
 function SelectionScreen() {
-  return <>Seçim ekranı</>;
+  return (
+    <div className="siparis-page">
+      <h2>Position Absolute Pizza</h2>
+      <div className="aciklama-bilgi">
+        <div className="para-puan">
+          <h1>85.50₺</h1>
+          <div>
+            <p>4.9</p>
+            <p>
+              <i className="fas fa-star"></i>
+              <i className="fas fa-star"></i>
+              <i className="fas fa-star"></i>
+              <i className="fas fa-star"></i>
+            </p>
+            <p>(200)</p>
+          </div>
+        </div>
+        <p>
+          Frontend Dev olarak hala position:absolute kullanıyorsan bu çok acı pizza tam sana göre. Pizza, domates, peynir ve genellikle çeşitli diğer malzemelerle kaplanmış, daha sonra geleneksel olarak odun ateşinde bir fırında yüksek sıcaklıkta pişirilen, genellikle yuvarlak, düzleştirilmiş mayalı buğday bazlı hamurdan oluşan İtalyan kökenli lezzetli bir yemektir. Küçük bir pizzaya bazen pizzetta denir.
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default SelectionScreen;
