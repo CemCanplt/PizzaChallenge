247425a10f237e52e9bd82e82c0039dd907a71d4
CemCanplt <cem--canpolat@hotmail.com>
2024-12-31 14:26:34 +0300

    Update SelectionScreen.jsx

--- src/Components/Pages/SelectionScreen.jsx
+++ src/Components/Pages/SelectionScreen.jsx
@@ -117,13 +117,7 @@ function SelectionScreen() {
               </div>
             </div>
             <p>
-              Frontend Dev olarak hala position:absolute kullanıyorsan bu çok
-              acı pizza tam sana göre. Pizza, domates, peynir ve genellikle
-              çeşitli diğer malzemelerle kaplanmış, daha sonra geleneksel olarak
-              odun ateşinde bir fırında yüksek sıcaklıkta pişirilen, genellikle
-              yuvarlak, düzleştirilmiş mayalı buğday bazlı hamurdan oluşan
-              İtalyan kökenli lezzetli bir yemektir. Küçük bir pizzaya bazen
-              pizzetta denir.
+              Frontend Dev olarak hala position:absolute kullanıyorsan bu çok acı pizza tam sana göre. Pizza, domates, peynir ve genellikle çeşitli diğer malzemelerle kaplanmış, daha sonra geleneksel olarak odun ateşinde bir fırında yüksek sıcaklıkta pişirilen, genellikle yuvarlak, düzleştirilmiş mayalı buğday bazlı hamurdan oluşan İtalyan kökenli lezzetli bir yemektir. Küçük bir pizzaya bazen pizzetta denir.
             </p>
           </div>
           <div className="boyut-hamur">
