cf4e75111406386710b9c3a102f0ec7d8029aefb
CemCanplt <cem--canpolat@hotmail.com>
2025-01-01 17:28:48 +0300

    boyut ve hamur seçim tuşları css güncellemesi

--- src/Components/Pages/SelectionScreen.jsx
+++ src/Components/Pages/SelectionScreen.jsx
@@ -105,169 +105,191 @@ function SelectionScreen({
   return (
     <>
       <div className="theSayfa">
-        <div className="sayfa-yolu">
+        {/* <div className="sayfa-yolu">
           <p>Anasayfa-Seçenekler-Sipariş Oluştur</p>
-        </div>
+        </div> */}
         <div className="siparis-page">
           <form onSubmit={handleSubmit}>
-            <h2>Position Absolute Pizza</h2>
-            <div className="aciklama-bilgi">
-              <div className="para-puan">
-                <h1>85.50₺</h1>
-                <div className="yildiz-skor">
-                  <p>4.9</p>
-                  <p>
-                    <i className="fas fa-star"></i>
-                    <i className="fas fa-star"></i>
-                    <i className="fas fa-star"></i>
-                    <i className="fas fa-star"></i>
-                    <i className="fas fa-star"></i>
-                  </p>
-                  <p>(200)</p>
-                </div>
-              </div>
-              <p>
-                Frontend Dev olarak hala position:absolute kullanıyorsan bu çok
-                acı pizza tam sana göre. Pizza, domates, peynir ve genellikle
-                çeşitli diğer malzemelerle kaplanmış, daha sonra geleneksel
-                olarak odun ateşinde bir fırında yüksek sıcaklıkta pişirilen,
-                genellikle yuvarlak, düzleştirilmiş mayalı buğday bazlı hamurdan
-                oluşan İtalyan kökenli lezzetli bir yemektir. Küçük bir pizzaya
-                bazen pizzetta denir.
-              </p>
-            </div>
-            <div className="boyut-hamur">
-              <div className="boyut">
-                <h2>Boyut Seç</h2>
-                <div>
-                  <input
-                    type="radio"
-                    id="kucukPizza"
-                    name="hamurSecimi"
-                    value="0"
-                    onChange={handleFiyat}
-                    defaultChecked
-                  ></input>
-                  <label htmlFor="kucukPizza"> Küçük</label>
-                </div>
-                <div>
-                  <input
-                    type="radio"
-                    id="ortaPizza"
-                    name="hamurSecimi"
-                    value="10"
-                    onChange={handleFiyat}
-                  ></input>
-                  <label htmlFor="ortaPizza"> Orta</label>
-                </div>
-                <div>
-                  <input
-                    type="radio"
-                    id="buyukPizza"
-                    name="hamurSecimi"
-                    value="20"
-                    onChange={handleFiyat}
-                  ></input>
-                  <label htmlFor="buyukPizza"> Büyük</label>
-                </div>
-              </div>
-              <div className="hamur">
-                <h2>Hamur Seç</h2>
-                <select id="hamurTipi" name="hamurTipi" onChange={handleFiyat}>
-                  <option value="Hamur Seçimi" selected disabled hidden>
-                    Hamur Kalınlığı
-                  </option>
-                  <option value="20">İnce Hamur</option>
-                  <option value="0">Orta Hamur</option>
-                  <option value="10">Kalın Hamur</option>
-                </select>
-              </div>
-            </div>
-            <div className="ek-malzeme">
-              <h2>Ek Malzemeler</h2>
-              <p>En fazla 10 malzeme seçebilirsiniz. 5₺</p>
-              <div className="checkbox">
-                {malzemeler.map((malzeme) => {
-                  return (
-                    <Checkbox
-                      setFormData={setFormData}
-                      formData={formData}
-                      malzeme={malzeme}
-                      key={malzeme}
-                      handleFiyat={handleFiyat}
-                      isDisabled={isDisabled}
-                      checked={formData.ekMalzeme.includes(
-                        malzeme.toLowerCase()
-                      )}
-                    />
-                  );
-                })}
-              </div>
-            </div>
-            <div className="isminiz">
-              <label htmlFor="ismim">
-                <h2>İsminiz</h2>
-              </label>
-              <input
-                type="text"
-                name="ismim"
-                id="ismim"
-                onChange={handleFiyat}
-                value={formData.ismim}
+            <div className="krema-arkaplan">
+              <img
+                src="../images/iteration-2-images/pictures/form-banner.png"
+                alt="form-banner"
               />
-              {adError && (
-                <p className="uyari-ad">İsminiz 3 karakterden kısa olamaz.</p>
-              )}
-            </div>
-            <div className="siparis-notu">
-              <label htmlFor="not">
-                <h2>Sipariş Notu</h2>
-              </label>
-              <textarea
-                name="not"
-                id="not"
-                placeholder="Siparişine eklemek istediğin bir not var mı?"
-                value={formData.not}
-                onChange={handleFiyat}
-              ></textarea>
-            </div>
-            <div className="divider"></div>
-
-            <div className="odeme-bolumu">
-              <div className="adet-sayaci">
-                <button name="eksi" onClick={counterHandler} type="button">
-                  -
-                </button>
-                <p>{counter}</p>
-                <button name="artı" onClick={counterHandler} type="button">
-                  +
-                </button>
-              </div>
-              <div className="siparis-fiyat-odeme">
-                <div className="siparis-hesap">
-                  <h4>Sipariş Toplamı</h4>
-                  <div className="row">
-                    <span>Seçimler</span>
-                    <span>{ekUcret}₺</span>
-                  </div>
-                  <div className="row-total">
-                    <span>Toplam</span>
-                    <span>{toplamUcret}₺</span>
+              <h2>Position Absolute Pizza</h2>
+              <div className="aciklama-bilgi">
+                <div className="para-puan">
+                  <h1>85.50₺</h1>
+                  <div className="yildiz-skor">
+                    <p>4.9</p>
+                    <p>
+                      <i className="fas fa-star"></i>
+                      <i className="fas fa-star"></i>
+                      <i className="fas fa-star"></i>
+                      <i className="fas fa-star"></i>
+                      <i className="fas fa-star"></i>
+                    </p>
+                    <p>(200)</p>
                   </div>
                 </div>
-                <div className="siparis-ver-tusu">
-                  <div className="gizli-adet-sayaci">
-                    <button name="eksi" onClick={counterHandler} type="button">
-                      -
-                    </button>
-                    <p>{counter}</p>
-                    <button name="artı" onClick={counterHandler} type="button">
-                      +
+                <p>
+                  Frontend Dev olarak hala position:absolute kullanıyorsan bu
+                  çok acı pizza tam sana göre. Pizza, domates, peynir ve
+                  genellikle çeşitli diğer malzemelerle kaplanmış, daha sonra
+                  geleneksel olarak odun ateşinde bir fırında yüksek sıcaklıkta
+                  pişirilen, genellikle yuvarlak, düzleştirilmiş mayalı buğday
+                  bazlı hamurdan oluşan İtalyan kökenli lezzetli bir yemektir.
+                  Küçük bir pizzaya bazen pizzetta denir.
+                </p>
+              </div>
+            </div>
+            <div className="kremasiz-arkaplan">
+              <div className="boyut-hamur">
+                <div className="boyut">
+                  <h2>Boyut Seç</h2>
+                  <div className="boyut-sec">
+                    <div className="label-flex">
+                      <input
+                        type="radio"
+                        id="kucukPizza"
+                        name="hamurSecimi"
+                        value="0"
+                        onChange={handleFiyat}
+                        defaultChecked
+                      ></input>
+                      <label htmlFor="kucukPizza">S</label>
+                    </div>
+                    <div className="label-flex">
+                      <input
+                        type="radio"
+                        id="ortaPizza"
+                        name="hamurSecimi"
+                        value="10"
+                        onChange={handleFiyat}
+                      ></input>
+                      <label htmlFor="ortaPizza">M</label>
+                    </div>
+                    <div className="label-flex">
+                      <input
+                        type="radio"
+                        id="buyukPizza"
+                        name="hamurSecimi"
+                        value="20"
+                        onChange={handleFiyat}
+                      ></input>
+                      <label htmlFor="buyukPizza">L</label>
+                    </div>
+                  </div>
+                </div>
+                <div className="hamur">
+                  <h2>Hamur Seç</h2>
+                  <select
+                    id="hamurTipi"
+                    name="hamurTipi"
+                    onChange={handleFiyat}
+                  >
+                    <option value="Hamur Seçimi" selected disabled hidden>
+                      -Hamur Kalınlığı Seç-
+                    </option>
+                    <option value="20">İnce Hamur</option>
+                    <option value="0">Orta Hamur</option>
+                    <option value="10">Kalın Hamur</option>
+                  </select>
+                </div>
+              </div>
+              <div className="ek-malzeme">
+                <h2>Ek Malzemeler</h2>
+                <p>En fazla 10 malzeme seçebilirsiniz. 5₺</p>
+                <div className="checkbox">
+                  {malzemeler.map((malzeme) => {
+                    return (
+                      <Checkbox
+                        setFormData={setFormData}
+                        formData={formData}
+                        malzeme={malzeme}
+                        key={malzeme}
+                        handleFiyat={handleFiyat}
+                        isDisabled={isDisabled}
+                        checked={formData.ekMalzeme.includes(
+                          malzeme.toLowerCase()
+                        )}
+                      />
+                    );
+                  })}
+                </div>
+              </div>
+              <div className="isminiz">
+                <label htmlFor="ismim">
+                  <h2>İsminiz</h2>
+                </label>
+                <input
+                  type="text"
+                  name="ismim"
+                  id="ismim"
+                  onChange={handleFiyat}
+                  value={formData.ismim}
+                />
+                {adError && (
+                  <p className="uyari-ad">İsminiz 3 karakterden kısa olamaz.</p>
+                )}
+              </div>
+              <div className="siparis-notu">
+                <label htmlFor="not">
+                  <h2>Sipariş Notu</h2>
+                </label>
+                <textarea
+                  name="not"
+                  id="not"
+                  placeholder="Siparişine eklemek istediğin bir not var mı?"
+                  value={formData.not}
+                  onChange={handleFiyat}
+                ></textarea>
+              </div>
+              <div className="divider"></div>
+
+              <div className="odeme-bolumu">
+                <div className="adet-sayaci">
+                  <button name="eksi" onClick={counterHandler} type="button">
+                    -
+                  </button>
+                  <p>{counter}</p>
+                  <button name="artı" onClick={counterHandler} type="button">
+                    +
+                  </button>
+                </div>
+                <div className="siparis-fiyat-odeme">
+                  <div className="siparis-hesap">
+                    <h4>Sipariş Toplamı</h4>
+                    <div className="row">
+                      <span>Seçimler</span>
+                      <span>{ekUcret}₺</span>
+                    </div>
+                    <div className="row-total">
+                      <span>Toplam</span>
+                      <span>{toplamUcret}₺</span>
+                    </div>
+                  </div>
+                  <div className="siparis-ver-tusu">
+                    <div className="gizli-adet-sayaci">
+                      <button
+                        name="eksi"
+                        onClick={counterHandler}
+                        type="button"
+                      >
+                        -
+                      </button>
+                      <p>{counter}</p>
+                      <button
+                        name="artı"
+                        onClick={counterHandler}
+                        type="button"
+                      >
+                        +
+                      </button>
+                    </div>
+                    <button onClick={() => setCurrentPage("finish")}>
+                      SİPARİŞ VER
                     </button>
                   </div>
-                  <button onClick={() => setCurrentPage("finish")}>
-                    SİPARİŞ VER
-                  </button>
                 </div>
               </div>
             </div>
